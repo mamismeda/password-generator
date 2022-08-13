656d2d1c1a45c814035e2e6dede6db17b1e0c7ba
mamismeda <amamismeda@gmail.com>
2022-08-13 18:26:46 +0400

    end of password generator

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ const randomFunc = {
   symbol: getRandomSymbol,
 };
 
+clipboardEl.addEventListener('click', () =>{
+    const textarea = document.createElement('textarea')
+    const password =resultEl.innerText
+
+    if(!password){ return }
+
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
+    alert('Password copied to clipboard!')
+ })
+
 generateEl.addEventListener("click", () => {
   const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;
@@ -69,4 +83,3 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(getRandomSymbol());
